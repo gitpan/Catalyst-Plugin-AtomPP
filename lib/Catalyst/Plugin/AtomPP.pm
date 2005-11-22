a1c2e50171f37fd23cc88b8949a2d24e889e3650
Daisuke Murase <typester@gmail.com>
2005-11-22 11:59:40 +0000

    Import of TYPESTER/Catalyst-Plugin-AtomPP-0.05_01 from CPAN.
    gitpan-cpan-distribution: Catalyst-Plugin-AtomPP
    gitpan-cpan-version:      0.05_01
    gitpan-cpan-path:         TYPESTER/Catalyst-Plugin-AtomPP-0.05_01.tar.gz
    gitpan-cpan-author:       TYPESTER
    gitpan-cpan-maturity:     developer

--- lib/Catalyst/Plugin/AtomPP.pm
+++ lib/Catalyst/Plugin/AtomPP.pm
@@ -1,10 +1,13 @@
 package Catalyst::Plugin::AtomPP;
 use strict;
+use base qw/Class::Accessor::Fast/;
+use Catalyst::Action;
 use Catalyst::Utils;
-use XML::Atom;
 use XML::Atom::Entry;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05_01';
+
+__PACKAGE__->mk_accessors(qw/_request_body_raw/);
 
 =head1 NAME
 
@@ -47,9 +50,30 @@ This plugin allows you to dispatch AtomPP methods with Catalyst.
 Require other authentication plugin, if needed.
 (Authentication::CDBI::Basic, WSSE, or so)
 
+=head1 AUTO RESPONSE FUTURE
+
+If you set true value at $c->config->{atompp}->{auto_response}, AtomPP plugin set automatically $c->res->status or $c->res->body by value that Remote method returned.
+
+If your remote method return /^\d{3}$/ ( 200 or so ), AtomPP plugin execute $c->res->status( 200 );
+
+Or return XML::Atom::Entry or XML::Atom::Feed object, execute $c->res->body( $xmlatom_obj->as_xml );
+
+Or other not false value returned, then execute $c->res->body( $returnd_value );
+
 =head1 METHODS
 
 =over 4
+
+=cut
+
+sub prepare_body_chunk {
+    my ( $c, $chunk ) = @_;
+
+    my $body = $c->request->{_body};
+    $body->add( $chunk );
+
+    $c->_request_body_raw( ( $c->_request_body_raw || '' ) . $chunk );
+}
 
 =item atom
 
@@ -57,9 +81,10 @@ Require other authentication plugin, if needed.
 
 sub atom {
     my $c = shift;
+    my $method = shift;
 
     my $class = caller(0);
-    (my $method = $c->req->action) =~ s!.*/!!;
+    ($method = $c->req->action) =~ s!.*/!! unless $method;
 
     my %prefixes = (
         POST   => 'create_',
@@ -71,45 +96,83 @@ sub atom {
     if (my $prefix = $prefixes{$c->req->method}) {
         $method = $prefix.$method;
     } else {
-        $c->log->debug(qq!Unsupported Method "@{[$c->req->method]}" called!);
+        $c->log->debug(qq!Unsupported Method "@{[$c->req->method]}" called!) if $c->debug;
         $c->res->status(501);
         return;
     }
 
-    $c->log->debug("Method: $method");
+    $c->log->debug("Method: $method") if $c->debug;
 
     if (my $code = $class->can($method)) {
-        my ($pp, $res);
+        my $pp;
 
-        for my $attr (@{Catalyst::Utils::attrs($code)}) {
+        for my $attr (@{ attributes::get($code) || [] }) {
             $pp++ if $attr eq 'Remote';
         }
 
         if ($pp) {
-            my $content = $c->req->body;
+            my $content = $c->_request_body_raw;
             my $entry;
-            $entry = XML::Atom::Entry->new(\$content) if $content;
+
+            eval{
+                $entry = XML::Atom::Entry->new( \$content );
+            };
+
+            $c->log->debug( $@ ) if ($c->debug and $@);
+
             if ($c->req->body and !$entry) {
-                $c->log->debug("Request body is not well-formed.");
+                $c->log->debug("Request body is not well-formed.") if $c->debug;
                 $c->res->status(415);
             } else {
                 $class = $c->components->{$class} || $class;
                 my @args = @{$c->req->args};
                 $c->req->args([$entry]) if $entry;
-                $c->actions->{reverse}->{$code} ||= "$class->$method";
-                $c->state($c->execute($class, $code));
+
+                my $name = ref $class || $class;
+                my $action = Catalyst::Action->new({
+                    name      => $method,
+                    code      => $code,
+                    reverse   => "-> $name->$method",
+                    class     => $name,
+                    namespace => Catalyst::Utils::class2prefix(
+                        $name, $c->config->{case_sensitive}
+                    ),
+                });
+                $c->state( $c->execute( $class, $action ) );
 
                 $c->res->content_type('application/xml; charset=utf-8');
+
+                # set status or body automaticaly
+                if ( $c->config->{atompp}->{auto_response} and $c->state ) {
+                    if ( $c->state =~ /^(\d{3})$/ ) {
+                        $c->log->debug("Auto Status: $1") if $c->debug;
+                        $c->res->status( $1 );
+                    }
+                    elsif ( ref($c->state) =~ /XML::Atom::(Feed|Entry)/ ) {
+                        my $xml = $c->state->as_xml;
+                        if ($] >= 5.008) {
+                            require Encode;
+                            Encode::_utf8_off( $xml );
+                        }
+                        $c->res->body( $xml );
+                    }
+                    else {
+                        $c->res->body( $c->state )
+                    }
+                }
+
                 $c->res->body($c->state);
                 $c->req->args(\@args);
             }
         }
 
         else {
-            $c->log->debug(qq!Method "$method" has no Atom attribute!);
+            $c->log->debug(qq!Method "$method" has no Atom attribute!) if $c->debug;
             $c->res->status(501);
         }
     }
+
+    $c->state;
 }
 
 =back
